f77925fd04da1a565b1f06e856cecb042a3af923
Baptiste Seux <seuxbaptiste@gmail.com>
2022-03-02 11:19:46 +0100

    fix async route + update readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import AdminRoute from "./components/routes/AdminRoute";
-import NotSignedInRoute from "./components/routes/NotSignedInRoute";
-import PremiumRoute from "./components/routes/PremiumRoute";
-import SignedInRoute from "./components/routes/SignedInRoute";
 import { AuthProvider } from "./providers/AuthProvider";
 import { CustomThemeProvider } from "./providers/CustomThemeProvider";
 import { SnackBarProvider } from "./providers/SnackbarProvider";
@@ -19,37 +15,69 @@ import Profile from "./views/Profile";
 import Users from "./views/Users";
 import WorkawayBot from "./views/workawayBot/WorkawayBot";
 
+const NotSignedInRoute = lazy(() => {
+  return Promise.all([
+    import("./components/routes/NotSignedInRoute"),
+    new Promise((resolve) => setTimeout(resolve, 200)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const PremiumRoute = lazy(() => {
+  return Promise.all([
+    import("./components/routes/PremiumRoute"),
+    new Promise((resolve) => setTimeout(resolve, 200)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const SignedInRoute = lazy(() => {
+  return Promise.all([
+    import("./components/routes/SignedInRoute"),
+    new Promise((resolve) => setTimeout(resolve, 200)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const AdminRoute = lazy(() => {
+  return Promise.all([
+    import("./components/routes/AdminRoute"),
+    new Promise((resolve) => setTimeout(resolve, 200)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+
 const App = () => {
   return (
     <CustomThemeProvider>
       <SnackBarProvider>
         <AuthProvider>
-          <Router>
-            <Switch>
-              <NotSignedInRoute exact path="/" component={Home} />
-              <NotSignedInRoute exact path="/signup" component={SignUp} />
-              <NotSignedInRoute exact path="/signin" component={SignIn} />
-              <NotSignedInRoute
-                exact
-                path="/reset-password"
-                component={ResetPassword}
-              />
+          {" "}
+          <Suspense fallback={<div>loading...</div>}>
+            <Router>
+              <Switch>
+                <NotSignedInRoute exact path="/" component={Home} />
+                <NotSignedInRoute exact path="/signup" component={SignUp} />
+                <NotSignedInRoute exact path="/signin" component={SignIn} />
+                <NotSignedInRoute
+                  exact
+                  path="/reset-password"
+                  component={ResetPassword}
+                />
 
-              <SignedInRoute exact path="/dashboard" component={Dashboard} />
-              <SignedInRoute exact path="/get-licence" component={GetLicence} />
-              <SignedInRoute exact path="/profile" component={Profile} />
+                <SignedInRoute exact path="/dashboard" component={Dashboard} />
+                <SignedInRoute
+                  exact
+                  path="/get-licence"
+                  component={GetLicence}
+                />
+                <SignedInRoute exact path="/profitle" component={Profile} />
 
-              <PremiumRoute
-                exact
-                path="/workaway-bot"
-                component={WorkawayBot}
-              />
+                <PremiumRoute
+                  exact
+                  path="/workaway-bot"
+                  component={WorkawayBot}
+                />
 
-              <AdminRoute exact path="/users" component={Users} />
+                <AdminRoute exact path="/users" component={Users} />
 
-              <Route path="/*" component={NotFound} />
-            </Switch>
-          </Router>
+                <Route path="/*" component={NotFound} />
+              </Switch>
+            </Router>
+          </Suspense>
         </AuthProvider>
       </SnackBarProvider>
     </CustomThemeProvider>
